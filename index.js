f1f2fe7bbb55ba5cefd70eefb27703d4ab43d52b
damilola <sewejed@gmail.com>
2021-09-17 11:09:51 +0100

    added start script

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+require("dotenv").config();
+
+const PORT = process.env.PORT;
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log("JSON Server is running");
 });
